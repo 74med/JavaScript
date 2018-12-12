a2efefddb8cea7c34f57cc5c1cfbac962c08d3a7
Mehdi <45572386+74med@users.noreply.github.com>
2018-12-12 21:43:32 +0100

    Add files via upload

---
+++ Exercice JavaScript/Js/app.js
@@ -0,0 +1,51 @@
+/* Echauffement 1
+var prenom = prompt("entrer votre non ");
+
+if (prenom.length >= 1  &&  prenom.length <= 10){
+    
+    alert("Bonjour, bienvenue " + prenom)
+   }
+
+else {
+    
+    alert(" veuillez reessayer ")
+
+}
+
+*/Echauffement 2
+        var cheval = prompt("De quelle couleur est le cheval blanc d'Henri 4 ?");
+        var blanc = "blanc";
+        var nain = 7;
+
+if (cheval === blanc) 
+    {
+         alert('Bravo bonne reponse')
+    }
+
+else 
+    {
+        alert('Mauvaise réponse')
+    } 
+        var nain = 7;
+        var nain = prompt("Combien sont les 7 nains ?");
+
+
+if ( nain === 7 ) 
+    {
+        alert('Bravo bonne reponse')
+        
+if else 
+    {
+        alert('mauvaise reponse')
+
+    }
+
+/* echauffement 3
+var identifiant = prompt("Veuillez entrer votre identifiant")
+var mdp = prompt ('Veuillez tapez votre mot de pass !')
+
+if (identifiant > 4 && identifiant != @ && identifiant != lea@gmail.com && = )
+
+
+
+*/
